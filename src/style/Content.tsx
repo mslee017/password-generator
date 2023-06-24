b9141d10d0dcc375de3117c202241f25517f9023
Matt <mslee017@yahoo.com>
2023-06-24 13:57:59 -0400

    Fixed Comments

--- src/style/Content.tsx
+++ src/style/Content.tsx
@@ -1,14 +1,6 @@
 import { Box, BoxProps } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 
-// const a = { x: 1, y: 2, z: 3 }
-// const { x, ...b } = a
-// // const x = a.x
-// console.log(x)
-// // 1
-// console.log(b)
-// // { y: 2, z: 3}
-
 export default function ContentStyle({
   children,
   ...styleProps
